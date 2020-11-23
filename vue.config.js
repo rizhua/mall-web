11497f3cc13d7a657cc4d36f03d9d84d4858b91a
huang <784256821@qq.com>
2020-11-23 18:31:12 +0800

    购物车，结算

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
         // 设置代理
         proxy: {
             "/api": {
-                target: "https://dev-s.haxima.cn", // 访问数据的计算机域名
+                target: "http://127.0.0.1:8000", // 访问数据的计算机域名
                 ws: false, // 是否启用websockets
                 changOrigin: true //开启代理
             }
