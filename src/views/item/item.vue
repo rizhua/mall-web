e299fa19415f37141b428f8d35d1fb0462adee24
huang <784256821@qq.com>
2020-12-26 18:49:01 +0800

    优化item页面

--- src/views/item/item.vue
+++ src/views/item/item.vue
@@ -5,13 +5,41 @@
 import Header from "@/components/header/header.vue";
 import Footer from "@/components/footer/footer.vue";
 
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
+
 export default {
   components: {
     "rz-header": Header,
     "rz-footer": Footer,
+    swiper,
+    swiperSlide,
   },
   data() {
     return {
+      specSwiperOption: {
+        speed: 200,
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        simulateTouch: false,
+        navigation: {
+          nextEl: ".spec-button-next",
+          prevEl: ".spec-button-prev",
+        },
+      },
+      tarckSwiperOption: {
+        loop: true,
+        speed: 200,
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 35,
+        simulateTouch: false,
+        direction: "vertical",
+        navigation: {
+          nextEl: ".tarck-button-next",
+          prevEl: ".tarck-button-prev",
+        },
+      },
       serverList: [
         { name: "正品保证", path: "" },
         { name: "门店自提", path: "" },
@@ -24,7 +52,21 @@ export default {
       sizeIndex: 0,
       imgIndex: 0,
       currentImgIndex: 0,
+      imageList: [
+        { src: require("../../assets/img/xiezi.jpg") },
+        { src: require("../../assets/img/xiezi1.jpg") },
+        { src: require("../../assets/img/xiezi2.jpg") },
+        { src: require("../../assets/img/xiezi3.jpg") },
+        { src: require("../../assets/img/xiezi4.jpg") },
+        { src: require("../../assets/img/xiezi5.jpg") },
+        { src: require("../../assets/img/xiezi6.jpg") },
+      ],
     };
+  },
+  computed: {
+    currentImage() {
+      return this.imageList[this.currentImgIndex].src;
+    },
   },
   methods: {
     handleChange(type) {
@@ -38,31 +80,16 @@ export default {
       }
     },
 
-    onMore(type) {
-      if (type == "prev") {
-        this.$refs.trackRef.prev();
-      } else {
-        this.$refs.trackRef.next();
-      }
+    addClass(idx) {
+      this.currentImgIndex = idx;
     },
 
-    onToggle(type) {
-      if (type == "prev") {
-        this.$refs.specRef.prev();
-      } else {
-        this.$refs.specRef.next();
-      }
+    toggleSize(idx) {
+      this.sizeIndex = idx;
     },
 
-    addClass(index) {
-      this.currentImgIndex = index;
-    },
-
-    toggleSize(index) {
-      this.sizeIndex = index;
-    },
-    toggleImg(index) {
-      this.imgIndex = index;
+    toggleImg(idx) {
+      this.imgIndex = idx;
     },
   },
 };
